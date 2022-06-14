b0345fc8f0379ee4497ed53f163e77a2b2dd82a7
gonzales54 <gonzalesitokawa@gmail.com>
2022-06-14 00:02:04 +0000

    top page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,11 +9,9 @@
 
     <body>
 
-        {{-- ナビゲーションバー --}}
         @include('commons.navbar')
 
         <div class="container">
-            {{-- エラーメッセージ --}}
             @include('commons.error_messages')
 
             @yield('content')
