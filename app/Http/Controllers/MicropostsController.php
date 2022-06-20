fb08249e7808d994ee8d7e6789c6022c55426b3b
gonzales54 <gonzalesitokawa@gmail.com>
2022-06-20 02:27:32 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -6,15 +6,12 @@ use Illuminate\Http\Request;
 
 class MicropostsController extends Controller
 {
-    public function index()
+public function index()
     {
         $data = [];
-        if (\Auth::check()) { // 認証済みの場合
-            // 認証済みユーザを取得
+        if (\Auth::check()) {
             $user = \Auth::user();
-            // ユーザの投稿の一覧を作成日時の降順で取得
-            // （後のChapterで他ユーザの投稿も取得するように変更しますが、現時点ではこのユーザの投稿のみ取得します）
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
                 'user' => $user,
